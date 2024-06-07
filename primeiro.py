314bfabc60a2e2664ca8c26a7b0eba43d4d49b27
SuporteCassia <princer37@gmail.com>
2024-06-07 13:32:35 -0300

    feature/TAR-001 adicionando calculo para mostrar valor total da compra

--- primeiro.py
+++ primeiro.py
@@ -2,3 +2,15 @@ print("*******************************************")
 print("               PROGRAMA PYTHON")
 print("*******************************************")
 
+valor_uni = float(input("Informe o valor unitário: "))
+quantidade = int(input("Informe a quantidade: "))
+totalCompra = valor_uni * quantidade
+
+print("O total da compra foi de : ", totalCompra)
+
+print("O total da compra foi de R$ {:.2f}".format(totalCompra))
+
+print(f'O total da compra foi de : {totalCompra:.2f}')
+
+
+
